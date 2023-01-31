26c89913eba211da9f9059d9163d8dbddb947ad0
Syed Hamza <syedhamzatahir1001@gmail.com>
2023-01-31 14:38:24 +0500

    Using Api to generate Suggestions

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,45 @@
 import React from "react"
 
+import axios from 'axios';
+
 export default function Main(){
 
+    
     const [topic,setTopic]=React.useState("")
+    const [suggestions,setSuggestions]=React.useState("Loading")
+    
+   
+
+React.useEffect( function(){
+
+    const options = {
+        method: 'POST',
+        url: 'https://api.cohere.ai/generate',
+        headers: {
+          accept: 'application/json',
+          'Cohere-Version': '2022-12-06',
+          'content-type': 'application/json',
+          authorization: 'Bearer BTLdlVK52xTi1DjvelK5C5mYzPqcoeildQ2FlMtA'
+        },
+        data: {
+            model:'command-xlarge-nightly',
+            prompt:'Suggest 3 topics to give a speech on\nSuggestions:\n',
+            max_tokens:300,
+            temperature:1.1,
+        }
+      };
+      
+      axios
+        .request(options)
+        .then(function (response) {
+            setSuggestions(response.data.generations[0].text)
+        })
+        .catch(function (error) {
+          //console.error(error);
+        });
+   
+},[])
+
 
     function handleInput(event)
     {
@@ -17,10 +54,10 @@ export default function Main(){
             <input type="text" onChange={handleInput} value={topic} placeholder="Type your Topic"></input>
             
             <div className="suggestions">
-            <h5>Some hot topics...</h5>
-            <button>Why Machine learning is important?</button>
-            <button>3 tips to reduce global warming</button>
-            <button>How to protect from is corona virus?</button></div>
+            <h5>Here are some topics for you to select</h5>
+            <div><p className="opt">{suggestions.split('\n')[1] || suggestions}</p></div>
+            <div><p className="opt">{suggestions.split('\n')[0] || suggestions}</p></div>
+            <div><p className="opt">{suggestions.split('\n')[2] || suggestions}</p></div></div>
         </section>
 
     </main>
